9c2cbc56d76054d6db414bc7f1b78bf8d17ebe81
Angelos Kolaitis <neoaggelos@gmail.com>
2025-05-05 22:12:02 +0300

    debug flanneld corruption
    Signed-off-by: Angelos Kolaitis <neoaggelos@gmail.com>

--- cmd/exp/image-builder/run_stage_post_run_commands.go
+++ cmd/exp/image-builder/run_stage_post_run_commands.go
@@ -1,11 +1,20 @@
 package main
 
-import "context"
+import (
+	"context"
+	"os"
+
+	"sigs.k8s.io/controller-runtime/pkg/log"
+)
 
 type stagePostRunCommands struct{}
 
 func (*stagePostRunCommands) name() string { return "post-run-commands" }
 
 func (*stagePostRunCommands) run(ctx context.Context) error {
+	log.FromContext(ctx).V(2).Info("Debugging post-run commands")
+
+	_ = client.RunCommand(ctx, cfg.instanceName, []string{"bash", "-c", "find /var/lib/containerd | grep flanneld | xargs -t sha256sum"}, nil, os.Stdout, os.Stderr)
+
 	return nil
 }
--- cmd/exp/image-builder/run_stage_snapshot_instance.go
+++ cmd/exp/image-builder/run_stage_snapshot_instance.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 )
 
 type stageCreateInstanceSnapshot struct{}
@@ -11,8 +12,12 @@ func (*stageCreateInstanceSnapshot) name() string { return "create-instance-snap
 
 // incus snapshot create capn-builder v0 --force
 func (*stageCreateInstanceSnapshot) run(ctx context.Context) error {
+	// note: wait to prevent disk corruption (?)
+	<-time.After(10 * time.Second)
+
 	if err := client.CreateInstanceSnapshot(ctx, cfg.instanceName, cfg.instanceSnapshotName); err != nil {
 		return fmt.Errorf("failed to delete instance: %w", err)
 	}
+	<-time.After(10 * time.Second)
 	return nil
 }
