6a6c77cc64c8f66c249f248f06c6d00449b757b9
Angelos Kolaitis <neoaggelos@gmail.com>
2025-05-10 17:49:08 +0300

    test binary
    Signed-off-by: Angelos Kolaitis <neoaggelos@gmail.com>

--- cmd/exp/image-builder/run_stage_stop_instance.go
+++ cmd/exp/image-builder/run_stage_stop_instance.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 )
 
 type stageStopInstance struct{}
@@ -11,6 +12,8 @@ func (*stageStopInstance) name() string { return "stop-instance" }
 
 // incus stop capn-builder
 func (*stageStopInstance) run(ctx context.Context) error {
+	<-time.After(30 * time.Second)
+
 	if err := client.StopInstance(ctx, cfg.instanceName); err != nil {
 		return fmt.Errorf("failed to stop instance: %w", err)
 	}
