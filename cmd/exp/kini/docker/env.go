d62267850708e6506ff7fec537ae6425519d8860
Angelos Kolaitis <neoaggelos@gmail.com>
2025-08-11 04:42:01 +0300

    improve handling of env variables
    Signed-off-by: Angelos Kolaitis <neoaggelos@gmail.com>

--- cmd/exp/kini/docker/env.go
+++ cmd/exp/kini/docker/env.go
@@ -3,6 +3,7 @@ package docker
 import (
 	"context"
 	"io"
+	"strconv"
 
 	"github.com/lxc/cluster-api-provider-incus/internal/lxc"
 )
@@ -20,19 +21,25 @@ type Environment struct {
 
 // Privileged returns true if user wants to launch privileged containers
 func (e *Environment) Privileged() bool {
-	return e.Getenv("KINI_UNPRIVILEGED") != "true"
+	if v, err := strconv.ParseBool(e.Getenv("KINI_UNPRIVILEGED")); err == nil {
+		return !v
+	}
+	return true
 }
 
 // KindInstances returns true if we must launch kind instances
 func (e *Environment) KindInstances(ctx context.Context) bool {
-	if e.Getenv("KINI_MODE") == "oci" {
-		return true
-	}
-
-	client, err := e.Client(ctx)
-	if err != nil {
+	switch e.Getenv("KINI_MODE") {
+	case "lxc":
 		return false
-	}
+	case "oci":
+		return true
+	default:
+		client, err := e.Client(ctx)
+		if err != nil {
+			return false
+		}
 
-	return client.SupportsInstanceOCI() == nil
+		return client.SupportsInstanceOCI() == nil
+	}
 }
