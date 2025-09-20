c2ff39af04859196e283bfebdb65229f09404dd7
Angelos Kolaitis <neoaggelos@gmail.com>
2025-09-20 23:11:54 +0300

    image-builder: compress rootfs.img of virtual machines on  (#153)
    Signed-off-by: Angelos Kolaitis <neoaggelos@gmail.com>

--- cmd/exp/simplestreams/internal/index/import_virtual_machine.go
+++ cmd/exp/simplestreams/internal/index/import_virtual_machine.go
@@ -20,7 +20,7 @@ import (
 	"sigs.k8s.io/yaml"
 )
 
-func (i *Index) importVirtualMachineUnifiedTarball(ctx context.Context, imagePath string, aliases []string, incus bool, lxd bool, compressRootfs bool) error {
+func (i *Index) importVirtualMachineUnifiedTarball(ctx context.Context, imagePath string, aliases []string, incus bool, lxd bool) error {
 	log.FromContext(ctx).Info("Importing virtual-machine image", "image", imagePath)
 
 	f, err := os.Open(imagePath)
@@ -101,14 +101,6 @@ func (i *Index) importVirtualMachineUnifiedTarball(ctx context.Context, imagePat
 
 	if metadata.Architecture == "" {
 		return fmt.Errorf("no metadata.yaml found in image")
-	}
-
-	// NOTE(neoaggelos): https://github.com/lxc/incus/commit/76804eedd6ac061fb4d974806be65ee78fb62c74
-	// Incus no longer compresses rootfs when exporting a unified tarball, so we have to
-	if compressRootfs && incus {
-		if outRootfs, err = qemuCompressImage(ctx, outRootfs); err != nil {
-			return fmt.Errorf("failed to compress qcow2 rootfs: %w", err)
-		}
 	}
 
 	// we now have:
